ae967e6368524ba23d1efa663ff9b074705e8f69
Lucas Fernandes <lfaa2057@gmail.com>
2023-09-11 22:48:12 -0300

    Clean main
    main cleaned

--- main.c
+++ main.c
@@ -1,24 +1,7 @@
-#include <stdio.h>
-#include "funcGame.h"
+#include "renderGame.h"
 
 int main()
 {   
-	char board[] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'}, mark, *pMark = &mark;
-
-
-	drawBoard(board);
-	for (int round = 0; round < 9; round++)
-	{
-		markBoard(round, board, pMark);
-		drawBoard(board);
-		if(checkWin(board, round, pMark) == 1)
-		{
-			printf("\nThe big winner is: %c\n", mark);
-			break;
-		}
-		else if(checkWin(board, round, pMark) == -1)
-			printf("A tie is a tie man...\n");
-	}
-
+	render();
 	return 0;
 }
---
+++ renderGame.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "funcGame.h"
+
+char board[] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'}, mark, *pMark = &mark;
+
+// Renderize the game with the hardcoded board
+void render()
+{
+	drawBoard(board);
+	for (int round = 0; round < 9; round++)
+	{
+		markBoard(round, board, pMark);
+		drawBoard(board);
+		if(checkWin(board, round, pMark) == 1)
+		{
+			printf("\nThe big winner is: %c\n", mark);
+			break;
+		}
+		else if(checkWin(board, round, pMark) == -1)
+			printf("A tie is a tie man...\n");
+	}
+}
